d01b72373fe3a94d172ef2c1d52083c1abc26e40
muryliang <muryliang@gmail.com>
2017-02-24 15:21:00 +0800

    udpate

---
+++ chapter2.1/Selection.java
@@ -0,0 +1,47 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Selection {
+    public static void sort (Comparable[] a) {
+        int max;
+        for (int i = 0; i < a.length-1; i++) {
+            max = i;
+            for (int j = i + 1; j < a.length; j++) 
+                if (less(a[j], a[max]))
+                        max = j;
+            exch(a, i, max);
+        }
+    }
+
+    private static boolean less (Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i]; 
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++ )
+            if (less(a[i], a[i-1])) return false;
+
+        return true;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++)
+            StdOut.print(a[i] + " ");
+        StdOut.println();
+    }
+
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
