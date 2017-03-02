776be8f8e6929d0b1993ad19ecbeb4ed86e5b891
muryliang <muryliang@gmail.com>
2017-03-02 13:20:37 +0800

    update

---
+++ chapter3/freqtest.java
@@ -0,0 +1,29 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.ST;
+
+/*show the most freq word*/
+
+public class freqtest {
+	public static void main(String[] args) {
+		int len = Integer.parseInt(args[0]);
+		ST<String, Integer> st = new ST<String, Integer>();
+
+		while (!StdIn.isEmpty()) {
+			String key = StdIn.readString();
+			if (key.length() < len) continue;
+			if (st.contains(key))
+				st.put(key, st.get(key) + 1);
+			else
+				st.put(key, 1);
+		}
+
+		String max = "";
+		st.put(max, 0);
+		for (String word : st)
+			if (st.get(word) > st.get(max))
+				max = word;
+		StdOut.println(max + " " + st.get(max));
+	}
+}
+
---
+++ chapter3/simpletest.java
@@ -0,0 +1,18 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.ST;
+
+/* show every word's last time index*/
+
+public class simpletest {
+	public static void main(String[] args) {
+		ST<String, Integer> st = new ST<String, Integer>();
+
+		for (int i = 0; !StdIn.isEmpty(); i++) {
+			String key = StdIn.readString();
+			st.put(key, i);
+		}
+		for (String s: st) 
+			StdOut.println(s + " " + st.get(s));
+	}
+}
