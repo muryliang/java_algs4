898130b1a6e4fe2ac0fae31299c08296203e2546
muryliang <muryliang@gmail.com>
2017-07-18 20:20:26 +0800

    udpate

--- algs4_new/percolation/Percolation.java
+++ algs4_new/percolation/Percolation.java
@@ -11,23 +11,27 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private final int line;
+    private boolean percolated;
     private int opened;
+    /* 
+     * 0: blocked
+     * 1: open
+     * 2: top_combined
+     * 4: bottom_combined
+     */
     private byte[] status;
     private final WeightedQuickUnionUF qf;
-    private final WeightedQuickUnionUF ff;
 
     public Percolation(int n) {
         if (n <= 0) throw new IllegalArgumentException("can not allocate less than 0");
-        status  = new byte[n*n+2];
+        status  = new byte[n*n];
         line = n;
         opened = 0;
-        qf = new WeightedQuickUnionUF(n*n+2);
-        ff = new WeightedQuickUnionUF(n*n+2);
-        for (int i = 0; i < line*line+2; i++) {
+        percolated = false;
+        qf = new WeightedQuickUnionUF(n*n);
+        for (int i = 0; i < line*line; i++) {
             status[i] = 0; // initialized to not opened
         }
-        status[line*line] = 1;
-        status[line*line+1] = 1;
     }
 
     private void checkIndex(int row, int col) {
@@ -39,46 +43,47 @@ public class Percolation {
         checkIndex(row, col);
         if (isOpen(row, col))
             return;
+        byte[] cur_status = {0,0,0,0};
 
-        status[(row-1)*line + col-1] = 1;
-        if (row == 1) {
-            qf.union((row-1)*line+col-1, line*line);
-            ff.union((row-1)*line+col-1, line*line);
-        }
+        byte tmpvar = 1;
+        if (row == 1) tmpvar |= 2;
+        if (row == line) tmpvar |= 4;
+        status[(row-1)*line+col-1] = tmpvar;
 
         if (row > 1 && isOpen(row-1, col)) {
+            cur_status[0] = status[qf.find((row-2)*line + col-1)];
             qf.union((row-1)*line+col-1, (row-2)*line+col-1);
-            ff.union((row-1)*line+col-1, (row-2)*line+col-1);
         }
 
         if (row < line && isOpen(row+1, col)) {
+            cur_status[1] = status[qf.find(row*line + col-1)];
             qf.union((row-1)*line+col-1, row*line+col-1);
-            ff.union((row-1)*line+col-1, row*line+col-1);
         }
         if (col > 1 && isOpen(row, col-1)) {
+            cur_status[2] = status[qf.find((row-1)*line + col-2)];
             qf.union((row-1)*line+col-1, (row-1)*line+col-2);
-            ff.union((row-1)*line+col-1, (row-1)*line+col-2);
         }
 
         if (col < line && isOpen(row, col+1)) {
-            qf.union((row-1)*line+col-1, (row-1)*line+col);
-            ff.union((row-1)*line+col-1, (row-1)*line+col);
+            cur_status[3] = status[qf.find((row-1)*line + col)];
+            qf.union((row-1)*line+col-1, (row-1)*line + col);
         }
 
-        // bottom line's unit is full, so connect to the last virtual
-        if (row == line)
-            qf.union((row-1)*line+col-1, line*line+1);
+        status[qf.find((row-1)*line+col-1)] = (byte)(tmpvar | cur_status[0] | cur_status[1] | cur_status[2] | cur_status[3]);
+        if (status[qf.find((row-1)*line+col-1)] == 7)
+           percolated = true;
+
         opened += 1;
     }
 
     public boolean isOpen(int row, int col) {
         checkIndex(row, col);
-        return status[(row-1)*line + col-1] == 1;
+        return status[(row-1)*line + col-1] != 0;
     }
 
     public boolean isFull(int row, int col) {
         checkIndex(row, col);
-        return isOpen(row, col) && ff.connected(line*line, (row-1)*line + col-1);
+        return (status[qf.find((row-1)*line + col-1)] & 3) == 3;
     }
 
     public int numberOfOpenSites() {
@@ -86,7 +91,7 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        return qf.connected(line*line, line*line+1);
+        return percolated;
     }
 
     public static void main(String[] args) {
