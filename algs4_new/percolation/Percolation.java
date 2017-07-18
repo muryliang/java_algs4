bf5aab0405135f652fd8b389697c08f500f5ad93
muryliang <muryliang@gmail.com>
2017-07-18 17:32:12 +0800

    update

--- algs4_new/percolation/Percolation.java
+++ algs4_new/percolation/Percolation.java
@@ -1,3 +1,9 @@
+/******************************************************************************
+ *  Compilation: 
+ *  [optional lines]
+ *  Execution: 
+ *  [optional lines]
+ ******************************************************************************/
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
@@ -6,93 +12,100 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
     private final int line;
     private int opened;
-	private boolean[] status;
-	private final WeightedQuickUnionUF qf;
+    private byte[] status;
+    private final WeightedQuickUnionUF qf;
+    private final WeightedQuickUnionUF ff;
 
-	public Percolation(int n) {
-		if (n <= 0) throw new IllegalArgumentException("can not allocate less than 0");
-		status  = new boolean[n*n+2];
-		line = n;
-		opened = 0;
-		qf = new WeightedQuickUnionUF(n*n+2);
-		for (int i = 0; i < line*line+2; i++) {
-			status[i] = false; // initialized to not opened
-		}
-        status[line*line] = true;
-		status[line*line+1] = true;
-	}
-
-    private void checkIndex(int row, int col) {
-		if (row < 1 || row > line || col < 1 || col > line)
-			throw new IllegalArgumentException("can not get illegal position" + row + " " + col);
+    public Percolation(int n) {
+        if (n <= 0) throw new IllegalArgumentException("can not allocate less than 0");
+        status  = new byte[n*n+2];
+        line = n;
+        opened = 0;
+        qf = new WeightedQuickUnionUF(n*n+2);
+        ff = new WeightedQuickUnionUF(n*n+2);
+        for (int i = 0; i < line*line+2; i++) {
+            status[i] = 0; // initialized to not opened
+        }
+        status[line*line] = 1;
+        status[line*line+1] = 1;
     }
 
-	public void open(int row, int col) {
+    private void checkIndex(int row, int col) {
+        if (row < 1 || row > line || col < 1 || col > line)
+            throw new IllegalArgumentException("can not get illegal position" + row + " " + col);
+    }
+
+    public void open(int row, int col) {
         checkIndex(row, col);
-		if (isOpen(row, col))
-			return;
-		status[(row-1)*line + col-1] = true;
-        // once one of top or bottom line'e member is opened,
-        // activate the corresponding virtual node
-        if (row == 1) 
+        if (isOpen(row, col))
+            return;
+
+        status[(row-1)*line + col-1] = 1;
+        if (row == 1) {
             qf.union((row-1)*line+col-1, line*line);
+            ff.union((row-1)*line+col-1, line*line);
+        }
 
-		// connect to surround
-		if (row > 1 && isOpen(row-1, col))
-			qf.union((row-1)*line+col-1, (row-2)*line+col-1);
+        if (row > 1 && isOpen(row-1, col)) {
+            qf.union((row-1)*line+col-1, (row-2)*line+col-1);
+            ff.union((row-1)*line+col-1, (row-2)*line+col-1);
+        }
 
-		// if the connecting bottom one is in last line, connect to bottom virtual node if full
-		if (row < line && isOpen(row+1, col)) {
-			qf.union((row-1)*line+col-1, (row)*line+col-1);
-			if ((row + 1 == line) && isFull(row+1, col))
-				qf.union(line*line, line*line+1);
-		}
-		if (col > 1 && isOpen(row, col-1))
-			qf.union((row-1)*line+col-1, (row-1)*line+col-2);
-		if (col < line && isOpen(row, col+1))
-			qf.union((row-1)*line+col-1, (row-1)*line+col);
+        if (row < line && isOpen(row+1, col)) {
+            qf.union((row-1)*line+col-1, row*line+col-1);
+            ff.union((row-1)*line+col-1, row*line+col-1);
+        }
+        if (col > 1 && isOpen(row, col-1)) {
+            qf.union((row-1)*line+col-1, (row-1)*line+col-2);
+            ff.union((row-1)*line+col-1, (row-1)*line+col-2);
+        }
 
-		// bottom line's unit is full, so connect to the last virtual
-        if ((row == line) && isFull(row, col))
-            qf.union(line*line, line*line+1);
-		opened += 1;
-	}
+        if (col < line && isOpen(row, col+1)) {
+            qf.union((row-1)*line+col-1, (row-1)*line+col);
+            ff.union((row-1)*line+col-1, (row-1)*line+col);
+        }
 
-	public boolean isOpen(int row, int col) {
+        // bottom line's unit is full, so connect to the last virtual
+        if (row == line)
+            qf.union((row-1)*line+col-1, line*line+1);
+        opened += 1;
+    }
+
+    public boolean isOpen(int row, int col) {
         checkIndex(row, col);
-		return status[(row-1)*line + col-1];
-	}
+        return status[(row-1)*line + col-1] == 1;
+    }
 
-	public boolean isFull(int row, int col) {
+    public boolean isFull(int row, int col) {
         checkIndex(row, col);
-		return isOpen(row, col) && qf.connected(line*line, (row-1)*line + col-1);
-	}
+        return isOpen(row, col) && ff.connected(line*line, (row-1)*line + col-1);
+    }
 
-	public int numberOfOpenSites() {
-		return opened;
-	}
+    public int numberOfOpenSites() {
+        return opened;
+    }
 
-	public boolean percolates() {
-		return qf.connected(line*line, line*line+1);
-	}
+    public boolean percolates() {
+        return qf.connected(line*line, line*line+1);
+    }
 
-	public static void main(String[] args) {
-		int n = StdIn.readInt();
-		int x1, y1;
-		Percolation per = new Percolation(n);
-		while (true) {
-			// get random from [1,n]
-			x1 = StdRandom.uniform(n)+1;
-			y1 = StdRandom.uniform(n)+1;
-			if (!per.isOpen(x1, y1)) {
-				per.open(x1, y1);
-				StdOut.println("open " + x1 + " " + y1);
-				if (per.percolates()) {
-					StdOut.println("percolated now " + per.numberOfOpenSites() + "/" + n*n + " " +  (double) per.numberOfOpenSites()/(n*n));
-					break;
-				}
-			}
-		}
-	}
+    public static void main(String[] args) {
+        int n = StdIn.readInt();
+        int x1, y1;
+        Percolation per = new Percolation(n);
+        while (true) {
+            // get random from [1,n]
+            x1 = StdRandom.uniform(n)+1;
+            y1 = StdRandom.uniform(n)+1;
+            if (!per.isOpen(x1, y1)) {
+                per.open(x1, y1);
+                StdOut.println("open " + x1 + " " + y1);
+                if (per.percolates()) {
+                    StdOut.println("percolated now " + per.numberOfOpenSites() + "/" + n*n + " " +  (double) per.numberOfOpenSites()/(n*n));
+                    break;
+                }
+            }
+        }
+    }
 }
 
--- algs4_new/percolation/PercolationStats.java
+++ algs4_new/percolation/PercolationStats.java
@@ -1,3 +1,9 @@
+/******************************************************************************
+ *  Compilation: 
+ *  [optional lines]
+ *  Execution: 
+ *  [optional lines]
+ ******************************************************************************/
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdOut;
@@ -11,58 +17,62 @@ public class PercolationStats {
 
     public PercolationStats(int n, int trials) {
         if (trials <= 0) throw new IllegalArgumentException("try number error");
-		rate = new double[trials];
-		loop = trials;
-		int x1, y1;
-		for (int i = 0; i < trials; i++) {
-			Percolation per = new Percolation(n);
-			while (true) {
-				x1 = StdRandom.uniform(n)+1;
-				y1 = StdRandom.uniform(n)+1;
-				if (!per.isOpen(x1, y1)) {
-					per.open(x1, y1);
-					if (per.percolates()) {
-						rate[i] = (double) per.numberOfOpenSites()/(n*n);
-						break;
-					}
-				}
-			}
-		}
-	}
-			
-	public double mean() {
-		return StdStats.mean(rate);
-	}
+        rate = new double[trials];
+        loop = trials;
+        int x1, y1;
+        for (int i = 0; i < trials; i++) {
+            Percolation per = new Percolation(n);
+            while (true) {
+                x1 = StdRandom.uniform(n)+1;
+                y1 = StdRandom.uniform(n)+1;
+                if (!per.isOpen(x1, y1)) {
+                    per.open(x1, y1);
+                    if (per.percolates()) {
+                        rate[i] = (double) per.numberOfOpenSites()/(n*n);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+            
+    public double mean() {
+        if (Double.isNaN(dmean)) 
+            dmean = StdStats.mean(rate);
+        return dmean;
+    }
 
-	public double stddev() {
-		return StdStats.stddev(rate);
-	}
+    public double stddev() {
+        if (Double.isNaN(dstddev))
+            dstddev = StdStats.stddev(rate);
+        return dstddev;
+    }
 
-	public double confidenceLo() {
-		if (Double.isNaN(dstddev))
-			dstddev = StdStats.stddev(rate);
-		if (Double.isNaN(dmean))
-			dmean = StdStats.mean(rate);
-		return dmean - 1.96*dstddev/Math.sqrt(loop);
-	}
+    public double confidenceLo() {
+        if (Double.isNaN(dstddev))
+            dstddev = StdStats.stddev(rate);
+        if (Double.isNaN(dmean))
+            dmean = StdStats.mean(rate);
+        return dmean - 1.96*dstddev/Math.sqrt(loop);
+    }
 
-	public double confidenceHi() {
-		if (Double.isNaN(dstddev))
-			dstddev = StdStats.stddev(rate);
-		if (Double.isNaN(dmean))
-			dmean = StdStats.mean(rate);
-		return dmean + 1.96*dstddev/Math.sqrt(loop);
-	}
+    public double confidenceHi() {
+        if (Double.isNaN(dstddev))
+            dstddev = StdStats.stddev(rate);
+        if (Double.isNaN(dmean))
+            dmean = StdStats.mean(rate);
+        return dmean + 1.96*dstddev/Math.sqrt(loop);
+    }
 
-	public static void main(String[] args) {
-		int line = Integer.parseInt(args[0]);
-		int loop = Integer.parseInt(args[1]);
-		Stopwatch timer1 = new Stopwatch();
-		PercolationStats per = new PercolationStats(line, loop);
-		double time1 = timer1.elapsedTime();
-		StdOut.println("time                    = "+ time1);
-		StdOut.println("mean                    = "+ per.mean());
-		StdOut.println("stddev                  = "+ per.stddev());
-		StdOut.println("95% confidence interval = "+ "["+ per.confidenceLo()+"," +  per.confidenceHi() +"]");
-	}
+    public static void main(String[] args) {
+        int line = Integer.parseInt(args[0]);
+        int loop = Integer.parseInt(args[1]);
+        Stopwatch timer1 = new Stopwatch();
+        PercolationStats per = new PercolationStats(line, loop);
+        double time1 = timer1.elapsedTime();
+        StdOut.println("time                    = "+ time1);
+        StdOut.println("mean                    = "+ per.mean());
+        StdOut.println("stddev                  = "+ per.stddev());
+        StdOut.println("95% confidence interval = "+ "["+ per.confidenceLo()+"," +  per.confidenceHi() +"]");
+    }
 }
