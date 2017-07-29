7bb8a369939f17f8b01659371dacc91bf6d27fe1
muryliang <muryliang@gmail.com>
2017-07-29 23:34:57 +0800

    udpate

---
+++ algs4_new/week3/Quick.java
@@ -0,0 +1,28 @@
+public class Quick {
+    public static void partition(Comparable[] a, int lo, int hi) {
+        int i = lo, j = hi + 1;
+        while (true) {
+            while (less(a[++i], a[lo]))
+                if (i == hi) break;
+
+            while (less(a[lo], a[--j]))
+                if (j == lo) break;
+
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+    public static void sort(Comparable[] a, int lo, int hi) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length-1);
+    }
+
+    public static void sort(Comparable[] a, int lo, int hi) {
+        if (lo <= hi) return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j-1);
+        sort(a, j+1, hi);
+    }
+}
