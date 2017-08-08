af2eaff9664fd5bcd78343dda2d9288e5e1c2928
muryliang <muryliang@gmail.com>
2017-08-08 17:31:28 +0800

    update

--- algs4_new/week5/kdtree/KdTree.java
+++ algs4_new/week5/kdtree/KdTree.java
@@ -53,34 +53,92 @@ public class KdTree {
     }
 
     public void draw() {
-        for (Point2D p: set) {
-            p.draw();
-        }
+        pDraw(root);
     }
+
+    private void pdraw(Node node) {
+        node.draw();
+        node left = node.getLeft();
+        node right = node.getRight();
+        if (left != null) pdraw(left);
+        if (right != null) pdraw(right);
+    }
+
 
     public Iterable<Point2D> range(RectHV rect) {
         if (rect == null) throw new IllegalArgumentException();
         SET<Point2D> ps;
         ps = new SET<Point2D>();
-        for (Point2D p: set) {
-            if (rect.contains(p))
-                ps.add(p);
-        }
+        search(root, rect, ps);
         return ps;
+    }
+
+    private void search(Node tmp, RectHV rect, SET<Point2D> ps) {
+        if (tmp != null) {
+            if (rect.contains(tmp)) {
+                ps.add(tmp);
+                search(tmp.getLeft(), rect, ps);
+                search(tmp.getRight(), rect, ps);
+            }
+            else if (tmp.isHorizon()) {
+                if (tmp.y() < rect.ymin())
+                    search(tmp.getRight(), rect, ps);
+                else
+                    search(tmp.getLeft(), rect, ps);
+            }
+            else {
+                if (tmp.x() < rect.xmin())
+                    search(tmp.getRight(), rect, ps);
+                else
+                    search(tmp.getLeft(), rect, ps);
+            }
+        }
     }
 
     public Point2D nearest(Point2D p) {
         if (p == null) throw new IllegalArgumentException();
-        double min = 0;
-        Point2D minp = null;
-        for (Point2D tmpp: set) {
-            double tmpmin = tmpp.distanceTo(p);
-            if (tmpmin < min) {
-                min = tmpmin;
-                minp = tmpp;
+        double min = curNearest(p, root, Double.POSITIVE_INFINITY);
+        return min;
+    }
+
+    private double curNearest(Point2D p, Node tmp, double curMinDis) {
+        // if zero, just return
+        if (curMinDis == 0.0) return curMinDis;
+        double curmin =  tmp.distanceTo(p);
+        if (curmin > curMinDis)
+            curmin = curMinDis;
+        if (tmp.isHorizon()) {
+            if (p.y() <= tmp.y()) {
+                curmin = curNearest(p, tmp.getLeft(), curmin);
+                if (curmin > tmp.y() - p.y()) {
+                    curmin = curNearest(p, tmp.getRight(), curmin);
+                }
+                return curmin;
+            }
+            else {
+                curmin = curNearest(p, tmp.getRight(), curmin);
+                if (curmin > p.y() - tmp.y()) {
+                    curmin = curNearest(p, tmp.getLeft(), curmin);
+                }
+                return curmin;
             }
         }
-        return minp;
+        else {
+            if (p.x() <= tmp.x()) {
+                curmin = curNearest(p, tmp.getLeft(), curmin);
+                if (curmin > tmp.x() - p.x()) {
+                    curmin = curNearest(p, tmp.getRight(), curmin);
+                }
+                return curmin;
+            }
+            else {
+                curmin = curNearest(p, tmp.getRight(), curmin);
+                if (curmin > p.x() - tmp.x()) {
+                    curmin = curNearest(p, tmp.getLeft(), curmin);
+                }
+                return curmin;
+            }
+        }
     }
 
     public static void main(String[] args) { }
@@ -124,10 +182,24 @@ class Node {
     }
 
     public Node setRight(Node node) {
-        return this.right node;
+        return this.right = node;
     }
 
     public boolean equals(Node node) {
         return p.equals(node.p);
+    }
+
+    public double distanceTo(Point2D that) {
+        return p.distanceTo(that);
+    }
+
+    public void draw() {
+        p.draw();
+        if (isHorizon()) {
+            new Point2D(0, p.y()).drawTo(new Point2D(1, p.y()));
+        }
+        else {
+            new Point2D(p.x(), 0).drawTo(new Point2D(p.x(), 1));
+        }
     }
 }
